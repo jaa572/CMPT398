7b8518205e5a9d16e9aa3042a0807d1bc413be12
Cameron Johns <caj540@smac15.usask.ca>
2016-02-27 21:31:12 -0600

    added add goal view controller-- DOES NOT yet switch between views (add new goal button causes crash)

---
+++ habitApp/habitApp/AddGoalViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddGoalViewController.h
+//  habitApp
+//
+//  Created by Cameron Johns on 2016-02-27.
+//  Copyright (c) 2016 Cameron Johns. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AddGoalViewController : NSObject
+
+@end
